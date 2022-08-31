539ca3b3756650045a0cd0ad446b0c923af43a46
diwas2057 <73152194+diwas2057@users.noreply.github.com>
2022-08-31 18:21:19 +0545

    Create skills.js

---
+++ FourthQuestion/skills.js
@@ -0,0 +1,9 @@
+const Skills = () => {
+    return <ul style={{alignItems:"center", display:"flex", flexDirection:"column", fontWeight:"bold" }}>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>ReactJS</li>
+    </ul>;
+   };
+   export default Skills;
