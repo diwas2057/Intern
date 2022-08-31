b0efb45fa691958f2cf4c8c391efc391f14ad9c2
diwas2057 <73152194+diwas2057@users.noreply.github.com>
2022-08-31 18:22:46 +0545

    Create about.js

---
+++ FourthQuestion/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return <>
+    <div className="img" style={{border:"1px solid black", height:"100px", width:"100px", marginLeft:"45%", marginTop:"10px"}}>
+        This is my pretty face
+    </div>
+    <p style={{ textAlign: "center" }}>I just graduated from Academia International College, Gwarko. 
+    I'm a CSIT student. I'm currently looking for a place where I can grow as a react developer</p>;
+    
+    </>
+   };
+   export default About;
