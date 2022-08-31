0398647a9ef5effa033217c4a8231fc64e1f1931
diwas2057 <73152194+diwas2057@users.noreply.github.com>
2022-08-31 18:17:23 +0545

    Create third.js

---
+++ ThirdQuestion/third.js
@@ -0,0 +1,21 @@
+import react from 'react'
+import './App.css'
+
+const App = () =>{
+    return(
+    <>
+
+                <div className="dibuContainer">
+
+                    <div className = " widget widget1 "> Widget 1</div>
+                    <div className = " widget widget2 "> Widget 2</div>
+                    <div className = " widget widget3 "> Widget 3</div>
+                    <div className = " widget widget4 "> Widget 4</div>
+                    <div className = " widget widget5 "> Widget 5</div>
+                    <div className = " widget widget6 "> Widget 6</div>
+                </div>
+
+        </>)
+}
+
+export default App
